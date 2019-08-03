6306638dda59ebea75f6791e656911c4ca288d7a
dragoshk <dragos_gtr@hotmail.com>
2019-08-03 17:43:54 -0300

    transition

---
+++ 360OnVR/Assets/_Assets/Scripts/TransitionImage.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TransitionImage : MonoBehaviour
+{
+    public Image transitionImage;
+    [SerializeField] float speed = 2;
+    public Color newColor;
+    [SerializeField] bool doIt;
+    [SerializeField] bool fadeOut = false;
+
+    private void Update()
+    {
+        if (doIt)
+        {
+            if (fadeOut)
+            {
+                FadeOutTransition();
+            }
+            else
+            {
+                FadeInTransition();
+            }
+        }
+    }
+
+    void FadeInTransition()
+    {
+        float value = transitionImage.color.a;
+        value -= Time.deltaTime * speed;
+        newColor.a = value;
+        transitionImage.color = newColor;
+    }
+
+    void FadeOutTransition()
+    {
+        float value = transitionImage.color.a;
+        value += Time.deltaTime * speed;
+        newColor.a = value;
+        transitionImage.color = newColor;
+    }
+}
