1d15321828ca36f1c6f7b3af87510bf783b41528
dragoshk <dragos_gtr@hotmail.com>
2019-08-04 21:29:42 -0300

    Better camera controls

---
+++ 360OnVR/Assets/_Assets/Scripts/FPSCamera.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets._Assets.Scripts
+{
+    public class FPSCamera : MonoBehaviour
+    {
+        public float rotationSpeed = 50f;
+        float minimunX = -360f, maximunX = 360f, minimunY = -60f, maximunY = 60f;
+        float sensitiveX = 15f, sensitiveY = 15f;
+        float rotationX, rotationY;
+
+        public Camera cam;
+
+        private void Update()
+        {
+            MoveCamera();
+        }
+
+        void MoveCamera()
+        {
+            rotationX += Input.GetAxis("Mouse X") * sensitiveX * rotationSpeed * Time.deltaTime; 
+            rotationY += Input.GetAxis("Mouse Y") * sensitiveY * rotationSpeed * Time.deltaTime;
+
+            rotationX = Mathf.Clamp(rotationX, minimunX, maximunX);
+            rotationY = Mathf.Clamp(rotationY, minimunY, maximunY);
+
+            cam.transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+        }
+    }
+}
